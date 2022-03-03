c1b49f18e82892a5b73f00b74a297a924a230894
suryo <suryoatm@gmail.com>
2022-03-03 19:22:43 +0700

    add React Native Elements

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import type {Node} from 'react';
+import { Button, ThemeProvider } from 'react-native-elements';
 import {
   SafeAreaView,
   ScrollView,
@@ -60,33 +61,9 @@ const App: () => Node = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ThemeProvider>
+      <Button title="Hey!" />
+    </ThemeProvider>
   );
 };
 
